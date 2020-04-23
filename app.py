a297c37c62c79eebdecaf52832c0c8320c705601
Somar <somar.jaafar19@gmail.com>
2020-04-23 00:03:57 -0400

    Q1 done?

--- app.py
+++ app.py
@@ -12,14 +12,29 @@ app = Flask(__name__)
 # end of your survey FlaskForm
 
 
+
 @app.route('/')
 def exam():
-    return render_template('exam.html')
+    return render_template('exam.html', prev_link="", next_link="/q1")
 
 
 # TODO: Question 1: questions endpoints
 # Routes for the 4 questions templates starting on the next line
+@app.route('/q1')
+def q1():
+    return render_template('q1.html', prev_link="/", next_link="/q2")
 
+@app.route('/q2')
+def q2():
+    return render_template('q2.html', prev_link="/q1", next_link="/q3")
+
+@app.route('/q3')
+def q3():
+    return render_template('q3.html', prev_link="/q2", next_link="/q4")
+
+@app.route('/q4')
+def q4():
+    return render_template('q4.html', prev_link="/q3", next_link="")
 # End of the 4 questions templates
 
 
